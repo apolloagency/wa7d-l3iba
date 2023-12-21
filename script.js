51207a56341a5e749e0b4ccdc2372a691792608d
apolloagency <146778521+apolloagency@users.noreply.github.com>
2023-12-21 21:00:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const soundButton = document.getElementById('soundButton');
+const moveButton = document.getElementById('moveButton');
+const clickSound = new Audio('ha9-mchaa.mp3'); // Replace with your sound file path
+
+soundButton.addEventListener('click', () => {
+    clickSound.play();
+    // Here goes the code to handle form submission with sound...
+    // For example, you can add form validation or submission logic here.
+});
+
+
+
+
+
+
+
+
+
+
+
